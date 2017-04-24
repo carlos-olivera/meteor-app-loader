4190932e37253120564fe53327f02e7e000c82ae
Carlos Olivera Terrazas <carlos.olivera.t@gmail.com>
2017-04-24 10:53:37 -0400

    Update loader
    Test on Meteor status

--- app-loader.js
+++ app-loader.js
@@ -2,10 +2,13 @@ if (Meteor.isServer) {
 	Inject.rawHead("loader", Assets.getText('loader.html'));
 }
 
+
 if (Meteor.isClient) {
-	Meteor.startup(function() {
-		setTimeout(function() {
+	Meteor.autorun(function () {
+        if ((Meteor.status().status === "connected")  && ($("#inject-loader-wrapper").length)) {
+            setTimeout(function() {
 			$("#inject-loader-wrapper").fadeOut(500, function() { $(this).remove(); });
 		}, 500);
-	});
+	  }
+        });
 }
